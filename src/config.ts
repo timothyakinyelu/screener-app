2e64c2309910b3c5d04467e8fd704fcdde4bba9e
loverofMush <tolujamal1@gmail.com>
2021-05-29 23:21:29 +0100

    - import axios - setup axios api url

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,24 @@
 // set up axios
-export{}
+import Axios from 'axios';
+
+
+let url = ''
+let devUrl = 'localhost:5000/';
+let prodUrl = '';
+
+switch (process.env.NODE_ENV) {
+    case 'development':
+        url = `http://${devUrl}`
+        break;
+    case 'production':
+        url = `https://${prodUrl}`
+        break;
+    default:
+        break;
+}
+
+const CSApi = Axios.create({
+    baseURL: url
+});
+
+export { CSApi };
