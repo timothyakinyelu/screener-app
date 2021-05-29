76a9cd2036cf624a6354805951e39c3d7b2cc5c1
loverofMush <tolujamal1@gmail.com>
2021-05-29 22:55:15 +0100

    - imported BrowserRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Router>
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    </Router>,
+    document.getElementById('root')
 );
 
 const updateHandler = (registration: ServiceWorkerRegistration) => {
