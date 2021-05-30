dc120810a96c35e756ece04cc11d6974a4f79249
loverofMush <tolujamal1@gmail.com>
2021-05-30 23:43:51 +0100

    - added routes

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,29 +1,26 @@
-import React from 'react'
-import { Switch, Route } from 'react-router-dom'
-import Dashboard from './pages/cryptocurrency/Dashboard'
-import Home from './pages/Home'
-import { MainRoutes } from './__url__'
-
+import React, { Fragment } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Dashboard from './pages/cryptocurrency/Dashboard';
+import Home from './pages/Home';
+import Footer from './utils/Footer';
+import { MainRoutes } from './__url__';
 
 const Routes: React.FC = (): JSX.Element => {
     return (
-        <div className="appWrapper">
-            <div className="container">
-                <Switch>
-                    <Route
-                        exact
-                        path={`${MainRoutes.home}`}
-                        component={Home}
-                    />
-                    <Route
-                        exact
-                        path={`${MainRoutes.crypto_screener_dashboard}`}
-                        component={Dashboard}
-                    />
-                </Switch>
+        <Fragment>
+            <div className="appWrapper max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+                <div className="md:container md:mx-auto sm:container sm:mx-auto">
+                    <div className="md:flex">
+                        <Switch>
+                            <Route exact path={`${MainRoutes.home}`} component={Home} />
+                            <Route exact path={`${MainRoutes.crypto_screener_dashboard}`} component={Dashboard} />
+                        </Switch>
+                    </div>
+                </div>
             </div>
-        </div>
-    )
-}
+            <Footer />
+        </Fragment>
+    );
+};
 
 export default Routes;
