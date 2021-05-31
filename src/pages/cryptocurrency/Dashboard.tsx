f65d5744c16437c708587bb42e4c2daa01f45851
loverofMush <tolujamal1@gmail.com>
2021-06-01 00:16:26 +0100

    - updated Dashboard

--- src/pages/cryptocurrency/Dashboard.tsx
+++ src/pages/cryptocurrency/Dashboard.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
+import Content from '../../components/cryptocurrency/dashboard/Content';
+import CryptoTable from '../../components/cryptocurrency/dashboard/CryptoTable';
 import Header from '../../components/cryptocurrency/dashboard/Header';
 import MobileSideNav from '../../utils/MobileSideNav';
 
 const Dashboard: React.FC = (): JSX.Element => {
     return (
         <div className="grid grid-cols-6 gap-2">
-            <div className="col-span-3">
+            <div className="col-span-6">
                 <Header />
             </div>
-            <div className="col-start-1 col-span-3"></div>
-            <div className="col-end-4 col-span-3">
-                <MobileSideNav />
+            <div className="col-span-6">
+                <Content />
+                <CryptoTable />
             </div>
         </div>
     );
