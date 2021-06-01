6a35cdf98181bf311d9ce9070ae93d52bd42416f
loverofMush <tolujamal1@gmail.com>
2021-06-02 00:12:44 +0100

    - modifed tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,10 @@ module.exports = {
     purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {},
+        backgroundColor: theme=>({
+            'primary': '#0db1bd',
+            'secondary': '#6236ff'
+        })
     },
     variants: {
         extend: {},
