27de488dfe2cac7606995b34439bbf4c7317a3bc
loverofMush <tolujamal1@gmail.com>
2021-06-08 21:57:04 +0100

    - added new class

--- src/utils/FilterButton.tsx
+++ src/utils/FilterButton.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const FilterButton: React.FC = (): JSX.Element => {
     return (
-        <div className="text-center screener-submit screener__cta">
+        <div className="col-span-2 text-center screener-submit screener__cta">
             <button className="btn btn-primary btn-block waves-effect" type="submit">
                 <span className="js-screener-count"> 6437 currencies</span>
             </button>
