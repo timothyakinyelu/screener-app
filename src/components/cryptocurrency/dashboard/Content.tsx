c0999428ad234133f2f5a15629c05352b0e91fe7
loverofMush <tolujamal1@gmail.com>
2021-06-12 13:44:38 +0100

    - imported useState - handleSetPattern to set new pattern - map patterns to display input

--- src/components/cryptocurrency/dashboard/Content.tsx
+++ src/components/cryptocurrency/dashboard/Content.tsx
@@ -1,14 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../../../static/css/content.css';
 import FilterButton from '../../../utils/FilterButton';
 import FormFilter from '../../../utils/FormFilter';
 import SetPattern from '../../../utils/SetPattern';
 
+interface Pattern {
+    value: string | null;
+}
 const Content: React.FC = (): JSX.Element => {
+    const [patterns, setPatterns] = useState<Pattern[]>([]);
+    const [pattern, setPattern] = useState<Pattern>({ value: '' });
+
+    const handleSetPattern = (e: React.MouseEvent<HTMLInputElement, MouseEvent>): void => {
+        e.preventDefault();
+        const name = e.currentTarget.textContent;
+        const current_ = name?.replace(' ', '_').toLowerCase();
+        const tag = document.getElementById(current_ as string) as HTMLDivElement;
+        tag?.classList.add('hide');
+        const _pattern: Pattern = {
+            value: name,
+        };
+        setPattern(_pattern);
+    };
+
+    useEffect(() => {
+        // take action when isVisible Changed
+        if (Object.keys(pattern).length !== 0) {
+            setPatterns((patterns) => [pattern, ...patterns]);
+        }
+    }, [pattern]);
+
+    const _patterns = () => {
+        return patterns.map((pattern, key) => {
+            if (pattern.value !== '') {
+                return <SetPattern key={key} pattern={pattern.value} />;
+            }
+        });
+    };
+
     return (
         <div className="wallet-card">
-            {/* <div className="card-body p-3">
-                <div className="screener"> */}
             <form
                 className="js-screener-form js-search-form"
                 data-count-url="/cryptocurrency-screener/count"
@@ -21,18 +52,14 @@ const Content: React.FC = (): JSX.Element => {
                 <div className="screener__filters">
                     <div className="screener__filters__card card-block">
                         <div className="grid grid-cols-2 gap-2 screener__filters__orderable">
+                            {_patterns()}
                             {/* will take a props to determine what to display */}
-                            <SetPattern />
-                            <SetPattern />
-                            <SetPattern />
-                            <FormFilter />
+                            <FormFilter handleSetPattern={handleSetPattern} />
                             <FilterButton />
                         </div>
                     </div>
                 </div>
             </form>
-            {/* </div>
-            </div> */}
         </div>
     );
 };
